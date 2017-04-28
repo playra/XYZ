ebf6ebd1a5e52eb18529375a3c8cb6f3589ec206
Dmitriy Vasilev <xyz@MacBook-Pro-Dmitriy.local>
2017-04-28 17:10:44 +0300

    time mashine

--- app/constants.js
+++ app/constants.js
@@ -1,5 +1,5 @@
 export const STROKE_WIDTH = .1
 export const STROKE_COLOR = 'cyan'
 export const DEFAULT_COLOR = 'black'
-export const CHOSEN_COLOR = 'purple'
+export const CHOSEN_COLOR = 'gold'
 export const DISABLED_COLOR = 'rgba(10, 10, 10, 0.5)'
